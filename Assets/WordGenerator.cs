11a95546df0ed0581fbb2f82fdad8fdbca155405
githubbell10 <52224530+githubbell10@users.noreply.github.com>
2019-11-20 22:31:53 -0600

    Initial commit

---
+++ Assets/WordGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordGenerator : MonoBehaviour
+{
+    private static string[] wordList = {"food", "morning", "wax", "word1", "word2", "phone", "homework", "glasses", "laundry" };
+   public static string GetRandomWord ()
+    {
+        int randomIndex = Random.Range(0, wordList.Length);
+        string randomWord = wordList[randomIndex];
+
+        return randomWord;
+    }
+}
